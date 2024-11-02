bef0afe6f5c5248df8b5bff4c3f6d289de38c825
Julie Ng <me@julie.io>
2024-11-02 10:08:39 +0100

    wip: read JSON, log target output files

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Convert Review Checklists JSON to markdown (POC)
 
 What if… instead of dealing with much hardcoded JSON data from [github.com/Azure/review-checklists](https://github.com/Azure/review-checklists), we converted it into markdown for [Nuxt Content](https://content.nuxt.com/) and leveraged Nuxt's [built-in search functionality](https://content.nuxt.com/usage/search)?
+
+# Resources
+
+- [Node.js Docs > Learn > Reading files with Node.js](https://nodejs.org/en/learn/manipulating-files/reading-files-with-nodejs)
--- transform.js
+++ transform.js
@@ -1,1 +1,50 @@
-console.log('hello world')
+const fs = require('fs')
+
+const targetDir = __dirname + '/output'
+// console.log('targetDir', targetDir)
+
+
+fs.readFile('./data/alz_checklist.en.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+
+  const result = parseJSON(data)
+  // console.log('result.items?', result.items)
+
+ result.items.forEach((item) => {
+    const itemDir = `${_toLowerDashed(item.category)}/${_toLowerDashed(item.subcategory)}`
+    const itemFilename = `_${_itemIdToFilename(item.id)}.md`
+    console.log(`${itemDir}/${itemFilename}`)
+  })
+})
+
+// ****************************************
+
+function parseJSON (jsonStr) {
+  const parsed = JSON.parse(jsonStr)
+  // console.log(Object.keys(parsed))
+  return parsed
+}
+
+function _toLowerDashed (str) {
+  return str.toLowerCase().replace(/\s+/g, '-')
+}
+
+function _createFolder (folderName) {
+  // const folderName = '/Users/joe/test';
+  try {
+    if (!fs.existsSync(folderName)) {
+      fs.mkdirSync(folderName)
+      return
+    }
+  } catch (err) {
+    throw new Error('Could not create folder', err)
+    console.error(err)
+  }
+}
+
+function _itemIdToFilename (itemId) {
+  return itemId.replace('.', '-').toLowerCase()
+}
